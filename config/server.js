c50acb2957b48c0b874c967fe7a141355dc3212b
Mario Ortas <mario.ortas@outlook.com>
2021-09-02 16:22:12 +0200

    project setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b040dede83481870065e3a6a324600da'),
+    },
+  },
+});
