e55efa64ddf840259c07bec582a985cb6484fa4f
Mario Ortas <mario.ortas@outlook.com>
2021-09-02 16:59:54 +0200

    restructured folders, created php

---
+++ back/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bc5f87bc-a2fb-4dea-8cd3-3b6f04c31db2'
+};
---
+++ pi/fanoff.php
@@ -0,0 +1,12 @@
+<?php
+use PiPHP\GPIO\GPIO;
+use PiPHP\GPIO\Pin\PinInterface;
+
+// Create a GPIO object
+$gpio = new GPIO();
+
+// Retrieve pin 17 and configure it as an output pin
+$pin = $gpio->getOutputPin(17);
+
+// Set the value of the pin low (turn it off)
+$pin->setValue(PinInterface::VALUE_LOW);
