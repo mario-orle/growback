47844715ed0ac7141cc54777a1f3bd7d0e8a72b5
Mario Ortas <mario.ortas@outlook.com>
2021-09-02 17:44:21 +0200

    taka

---
+++ front/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ hello }}</div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    hello: gql`query {
+      climas {
+        created_at
+      }
+    }`,
+  },
+}
+</script>
