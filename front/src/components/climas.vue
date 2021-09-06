b9a020b6096c80f12cb356cfc5e78f87b83499d2
Mario Ortas <mario.ortas@outlook.com>
2021-09-06 13:29:07 +0200

    adjustments

--- front/src/components/climas.vue
+++ front/src/components/climas.vue
@@ -64,7 +64,7 @@ export default ({
               bounds: 'ticks',
               type: 'time',
               time: {
-                  unit: 'minute'
+                  unit: 'hour'
               }
             }],
             yAxes: [{
